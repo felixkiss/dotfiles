9a3beec2d341561e0f959ea682a1ba21bdfdbd13
Felix Kiss <felix@felixkiss.com>
2016-12-10 06:58:22 +0100

    Set some osx specific settings

--- zsh/platform-osx.sh
+++ zsh/platform-osx.sh
@@ -1,2 +1,9 @@
 # Add ~/.bin/osx to PATH
 [[ -d $HOME/.bin/osx ]] && export PATH=$HOME/.bin/osx:$PATH
+
+export EDITOR="text --wait"
+
+export HOMEBREW_NO_ANALYTICS=1
+
+# Register SSH key to agent
+ssh-add ~/.ssh/id_rsa
