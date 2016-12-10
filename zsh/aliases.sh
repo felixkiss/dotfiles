f2e0afb105c457d88156f6084797d5479972be64
Felix Kiss <felix@felixkiss.com>
2016-12-10 07:00:01 +0100

    Add some useful aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -4,8 +4,12 @@ alias gl='git log'
 
 # Laravel aliases
 alias art='php artisan'
+alias vm='ssh vagrant@127.0.0.1 -p 2222'
 
 # Vagrant aliases
 alias vs='vagrant status'
 alias vu='vagrant up'
 alias vd='vagrant destroy'
+
+# RubyGems aliases
+alias be='bundle exec'
