fe6abbd41b51716028f58dd578bf32563c6c5261
Felix Kiss <felix@felixkiss.com>
2018-08-15 18:34:15 +0200

    Add aliases to toggle desktop icons on macOS
    Source: https://twitter.com/miguelsolorio_/status/1026934460975960064

--- zsh/platform-osx.sh
+++ zsh/platform-osx.sh
@@ -7,3 +7,7 @@ export HOMEBREW_NO_ANALYTICS=1
 
 # Register SSH key to agent
 ssh-add ~/.ssh/id_rsa
+
+# Add aliases to toggle desktop files
+alias desktop-hide-files='defaults write com.apple.finder CreateDesktop false; killall Finder'
+alias desktop-show-files='defaults write com.apple.finder CreateDesktop true; killall Finder'
