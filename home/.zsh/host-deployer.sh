771ee53a1cb33cd30dded249b4cd0cf664fb3ebe
Felix Kiss <felix@felixkiss.com>
2021-09-26 07:05:26 +0200

    Go into /current automatically

--- home/.zsh/host-deployer.sh
+++ home/.zsh/host-deployer.sh
@@ -1,6 +1,6 @@
 if [[ "$USER" == "deploy" ]]; then
   # start in default app directory
-  cd $(ls -d /data/apps/* | head -n 1)
+  cd "$(ls -d /data/apps/* | head -n 1)/current" || cd /data/apps
 else
   alias deploy='sudo su - deploy'
 fi
